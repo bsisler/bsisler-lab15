6093b93804a760f63f1a1426e094aaabafcbe2c4
Briana <bsisler81@gmail.com>
2025-12-01 00:45:01 -0500

    Finished framework for GUI, labels, text areas, text fields, and comments

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -15,17 +15,31 @@ public class EnigmaFrame extends JFrame{
         enigmaFrame.setLocation(100, 100);              
         enigmaFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        //Buttons
+        //Buttons (b)
         JButton bEncrypt = new JButton("Encrypt");
+        JLabel lEncrypt = new JLabel("Encrypt");
         JButton bDecrypt = new JButton ("Decrypt");
+        JLabel lDecrypt = new JLabel("Decrypt");
         
-        //Combo Boxes
+        //Combo Boxes (combo)
         String rotorOptions[] = {"1", "2", "3", "4", "5"};
         JComboBox<String> comboInnerRotor = new JComboBox<String>(rotorOptions);
+        JLabel lInner = new JLabel("Inner");
         JComboBox<String> comboMiddleRotor = new JComboBox<String>(rotorOptions);
+        JLabel lMiddle = new JLabel("Middle");
         JComboBox<String> comboOuterRotor = new JComboBox<String>(rotorOptions);
+        JLabel lOuter = new JLabel("Out");
         
-        
+        //Text Field (tF)
+        JTextField tFStartPos = new JTextField();
+        JLabel lStart = new JLabel("Initial Positions");
+
+        //Text Areas (tA)
+        JTextArea tAInput = new JTextArea();
+        JLabel lInput = new JLabel("Input");
+        JTextArea tAOutput = new JTextArea();
+        JLabel lOutput = new JLabel("Output");
+
         
         
         
